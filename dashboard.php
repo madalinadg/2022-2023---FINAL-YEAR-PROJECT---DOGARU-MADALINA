5d7e6514a30aa328fbced1ffbf2dc0e90f4be60a
Madalina Dogaru <92085487+madalinadg@users.noreply.github.com>
2023-05-23 19:56:48 +0300

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <link rel="stylesheet" href="style-dsh.css">
+
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <!-- sidebard -->
+    <section id="sidebar">
+        <a href="#" class="brand"><img src="assets/logo.png" alt="Logo">MM Beauty Boutique</a>
+        <ul class="side-menu">
+            <li>
+                <a href="#" class="active"><i class='bx bxs-dashboard icon'></i> Dashboard</a></li>
+            <li class="divider">Main</li>
+            <li>
+                <a href="#"><i class='bx bxs-inbox icon'></i> Elements <i class='bx bx-chevron-right icon-right'></i></a>
+                <ul class="side-dropdown">
+                    <li><a href="#">Alert</a></li>
+                    <li><a href="#">Badges</a></li>
+                    <li><a href="#">Breadcrumbs</a></li>
+                    <li><a href="#">Button</a></li>
+                </ul>
+            </li>
+            <li><a href="#"><i class='bx bxs-chart icon'></i> Charts</a></li>
+            <li><a href="#"><i class='bx bxs-widget icon'></i> Widget</a></li>
+            <li class="divider">Table and forms</li>
+            <li><a href="#"><i class='bx bx-table icon'></i> Tables</a></li>
+            <li>
+                <a href="#"><i class='bx bxs-notepad icon'></i> Forms <i class='bx bx-chevron-right icon-right'></i></a>
+                <ul class="side-dropdown">
+                    <li><a href="#">Basic</a></li>
+                    <li><a href="#">Select</a></li>
+                    <li><a href="#">Checkbox</a></li>
+                    <li><a href="#">Radio</a></li>
+                </ul>
+            </li>
+        </ul>
+        <div class="ads">
+            <div class="wrapper">
+                <a href="#" class="btn-upgrade">adaugă csv</a>
+                <p>Adaugă <span>testele realizate</span> pe utilizatori</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- navbar -->
+    <section id="content">
+        <nav>
+            
+            <span class="divider"></span>
+            <div class="profile">
+                <ul class="profile-link">
+                    <li><a href="#"><i class='bx bxs-log-out-circle icon' ></i> Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        
+    <!-- main -->
+        <main>
+        <hl class="title">Dashboard</h1>
+        <ul class="breadcrumbs">
+            <li><a href="#">Home</a></li>
+            <li class="divider">/</li>
+            <li><a href="#" class="active">Dashboard</a></li>
+        </ul>
+
+     <!-- content -->
+
+
+    </main>
+
+    </section>
+
+
+    
+
+
+
+    <script src="script.js"></script>
+
+</body>
+</html>
